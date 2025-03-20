f07ae9323a5766e68d0b858d2bdced04c7e891b5
adriansanzito <76404853+adriansanzito@users.noreply.github.com>
2025-03-20 20:17:16 +0100

    MAIN MERGE
    merge suponemos que esta todo

--- Xtra_Xaotic_Xroll/Assets/SCRIPTS/PLAYER_CONTROLLER.cs
+++ Xtra_Xaotic_Xroll/Assets/SCRIPTS/PLAYER_CONTROLLER.cs
@@ -8,9 +8,6 @@ using TMPro;
 
 public class PLAYER_MOVEMENT : MonoBehaviour
 {
-    //public GameObject CoinVaso;
-    //public GameObject Ganar;
-    //public GameObject Flag;
     public float vel;
     public float jumpForce;
     int _jumpCount;
@@ -20,99 +17,108 @@ public class PLAYER_MOVEMENT : MonoBehaviour
     public float playerPos;
     public TextMeshProUGUI locationText;
     public GameObject playerWeapon;
-    public ENEMY_CONTROLLER ENEMY_CONTROLLER;
-    //int _coins;
-    //int _totalCoins;
+    public MINION_CONTROLLER MINION_CONTROLLER;
+    public GameObject drop;
+    public int _dropCount;
+    public TextMeshProUGUI resourceCounter;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Desactivamos el panel del Vault y pillamos el RigidBody
         vaultmenu.SetActive(false);
-        //Ganar.SetActive(false);
-        //Flag.SetActive(false);
         _rb = GetComponent<Rigidbody2D>();
-        //_totalCoins = CoinVaso.transform.childCount;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Estas 3 líneas copian la posición del player y la manda al minimapa
         playerPos = this.gameObject.transform.position.x;
         minimap.value = playerPos;
         locationText.text = playerPos.ToString("F0");
+
         float direction = Input.GetAxis("Horizontal");
+        if (direction != 0) // Mambiar la direccion del sprite segun su desplazamiento
+        {
+            FlipSprite(direction);
+        }
         _rb.velocity = new Vector2(vel * direction, _rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space) && _jumpCount <= 0)
+        if (Input.GetKeyDown(KeyCode.Space) && _jumpCount <= 0) // Movimiento basico del jugador
         {
             _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
             _jumpCount++;
         }
-        else { }
-        //if (_coins == _totalCoins)
-        //{
-        //    Flag.SetActive(true);
-        
     }
+    void FlipSprite(float direction)
+    {
+        // Si el player se mueve a la izquierda -1
+        if (direction < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Mscala invertida en vertical (flip)
+        }
+        // Si el player se mueve a la derecha +1
+        else if (direction > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Mantener la escala original
+        }
+    }
+
+
+    // Comprobamos los colliders y sus tags
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        // Cuando tocamos el suele, reseteamos la cuenta de saltos
         if (collision.collider.tag == "SUELO")
         {
             _jumpCount = 0;
         }
+
+        // Cuando tocamos la base enemiga, reseteamos la escena (provisional)
         if (collision.collider.tag == "ENEMYBASE")
         {
             SceneManager.LoadScene(0);
         }
     }
+
+    // Comprobamos la entrada en triggers y sus tags
     public void OnTriggerEnter2D(Collider2D other)
     {
+        // Cuando entramos en la base aliada, activamos el panel de vault
         if (other.tag == "YOURBASE")
         {
             vaultmenu.SetActive(true);
         }
     }
+
+    // Comprobamos la salida de triggers y sus tags
     public void OnTriggerExit2D(Collider2D other)
     {
+        // Cuando salimos de la base, desactivamos el panel de vault
         if (other.tag == "YOURBASE")
         {
             vaultmenu.SetActive(false);
         }
     }
+
+    // Comprobamos si estamos dentro de un trigger y sus tags
     public void OnTriggerStay2D(Collider2D other)
     {
+        // Si estamos dentro de un enemigo, hacemos clic y llamamos al conteo de vida del enemigo
         if(other.tag == "ENEMY" && Input.GetMouseButton(0)) 
         {
+            other.gameObject.GetComponent<MINION_CONTROLLER>().GetDamage();
             print("BONK");
-            other.gameObject.GetComponent<ENEMY_CONTROLLER>().GetDamage();
             //aqui si deja apretao se ejecuta 1000 veces por segundo, añade algun tipo de cooldown crack
         }
+
+        // Si estamos dentro de un drop, nos suma puntos y eliminamos el objeto
+        if (other.tag == "DROP")
+        {
+            _dropCount += 10;
+            print(_dropCount);
+            Destroy(other.gameObject);
+            resourceCounter.text = _dropCount.ToString("F0");
+        }
     }
-    //public void OnCollisionEnter2D(Collision2D collision)
-    //{
-
-    //if (collision.collider.tag == "SPIKE")
-    //{
-    //    SceneManager.LoadScene(0);
-    //}
-    //if (collision.collider.tag == "MUELTE")
-    //{
-    //    SceneManager.LoadScene(0);
-    //}
-
-    //}
-    //public void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.tag == "COIN")
-    //    {
-    //        _coins++;
-    //        Destroy(collision.gameObject);
-    //    }
-    //    if (collision.tag == "FLAG")
-    //    {
-    //        print("GANASTE NEN@");
-    //        SceneManager.LoadScene(0);
-    //        Ganar.SetActive(true);
-    //    }
-    //}
 }
