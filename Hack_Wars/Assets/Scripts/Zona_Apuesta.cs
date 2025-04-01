227761bf1f79b0f795d1bc0ef7e261db4734fd7a
AmberZxxiv <amberzxxiv@gmail.com>
2025-04-01 18:17:41 +0200

    Ahora se puede apostar
    en verdad no del todo, solo hacer clic en botones

--- Hack_Wars/Assets/Scripts/Zona_Apuesta.cs
+++ Hack_Wars/Assets/Scripts/Zona_Apuesta.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,38 @@ public class Zona_Apuesta : MonoBehaviour
 {
     public GameObject zonaApuesta;
     public int timeChange = 5; // tiempo cada que se activa el modo apuesta
+    public bool rojo;
+    public bool negro;
+    public bool verde;
+    public bool apuestaHecha;
+    public int result;
 
     // Start is called before the first frame update
     void Start()
     {
         zonaApuesta.SetActive(false);
         StartCoroutine(TimeApuesta());
+        apuestaHecha = false;
+        rojo = false;
+        negro = false;
+        verde = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(rojo==true && result==1)
+        {
+            print("rojo");
+        }
+        if(negro==true && result==2)
+        {
+            print("negro");
+        }
+        if(verde==true && result==3)
+        {
+            print("verde");
+        }
     }
 
     IEnumerator TimeApuesta() // cada (timeChange) segundos se activa el modo apuesta en la partida
@@ -25,14 +46,41 @@ public class Zona_Apuesta : MonoBehaviour
         yield return new WaitForSeconds(timeChange);
         zonaApuesta.SetActive(true);
         Time.timeScale = 0;
-        
+
     }
 
+    // Boton para pasar a la siguiente fase
     public void Next_Round ()
     {
         // cargar siguiente fase de boss
         zonaApuesta.SetActive(false);
         Time.timeScale = 1;
         StartCoroutine(TimeApuesta());
+    }
+
+    // 3 Funciones para apostar a las 3 opciones diferentes
+    public void Apuesta_rojo()
+    {
+        Ruleta_time();
+        rojo = true;
+        apuestaHecha=true;
+    }
+    public void Apuesta_negro()
+    {
+        Ruleta_time();
+        negro = true;
+        apuestaHecha = true;
+    }
+    public void Apuesta_verde()
+    {
+        Ruleta_time();
+        verde = true;
+        apuestaHecha = true;
+    }
+
+    // Genera un numero aleatoria del 1 al 3
+    public void Ruleta_time()
+    {
+        result = Random.Range(1, 3);
     }
 }
